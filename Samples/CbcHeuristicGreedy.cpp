1d50c5a6b2c740cb5f03c2e9e4e5b9fa78bbd063
John Forrest <john.forrest@fastercoin.com>
2005-05-20 15:48:21 +0000

    add equality

--- Samples/CbcHeuristicGreedy.cpp
+++ Samples/CbcHeuristicGreedy.cpp
@@ -10,8 +10,10 @@
 
 #include "OsiSolverInterface.hpp"
 #include "CbcModel.hpp"
+#include "CbcStrategy.hpp"
 #include "CbcHeuristicGreedy.hpp"
 #include "CoinSort.hpp"
+#include "CglPreProcess.hpp"
 // Default Constructor
 CbcGreedyCover::CbcGreedyCover() 
   :CbcHeuristic()
@@ -19,6 +21,7 @@ CbcGreedyCover::CbcGreedyCover()
   // matrix  will automatically be empty
   originalNumberRows_=0;
   algorithm_=0;
+  numberTimes_=100;
 }
 
 // Constructor from model
@@ -30,6 +33,7 @@ CbcGreedyCover::CbcGreedyCover(CbcModel & model)
   matrix_ = *model.solver()->getMatrixByCol();
   originalNumberRows_=model.solver()->getNumRows();
   algorithm_=0;
+  numberTimes_=100;
 }
 
 // Destructor 
@@ -50,7 +54,8 @@ CbcGreedyCover::CbcGreedyCover(const CbcGreedyCover & rhs)
   CbcHeuristic(rhs),
   matrix_(rhs.matrix_),
   originalNumberRows_(rhs.originalNumberRows_),
-  algorithm_(rhs.algorithm_)
+  algorithm_(rhs.algorithm_),
+  numberTimes_(rhs.numberTimes_)
 {
   this->setWhen(rhs.when());
 }
@@ -64,6 +69,7 @@ CbcGreedyCover::operator=( const CbcGreedyCover& rhs)
     matrix_=rhs.matrix_;
     originalNumberRows_=rhs.originalNumberRows_;
     algorithm_=rhs.algorithm_;
+    numberTimes_=rhs.numberTimes_;
   }
   return *this;
 }
@@ -77,7 +83,12 @@ CbcGreedyCover::solution(double & solutionValue,
   // See if to do
   if (!when()||(when()==1&&model_->phase()!=1))
     return 0; // switched off
-  if (model_->getNodeCount()>500)
+  if (model_->getNodeCount()>numberTimes_)
+    return 0;
+  // See if at root node
+  bool atRoot = model_->getNodeCount()==0;
+  int passNumber = model_->getCurrentPassNumber();
+  if (atRoot&&passNumber!=1)
     return 0;
   OsiSolverInterface * solver = model_->solver();
   const double * columnLower = solver->getColLower();
@@ -218,6 +229,9 @@ CbcGreedyCover::solution(double & solutionValue,
             // add to next time
             which[newNumber++]=iColumn;
 	    double ratio = (cost/sum)*(1.0+perturb*CoinDrand48());
+            // If at root choose first
+            if (atRoot)
+              ratio = iColumn;
 	    if (ratio<bestRatio) {
               bestRatio=ratio;
               bestColumn=iColumn;
@@ -380,6 +394,464 @@ CbcGreedyCover::validate()
            j<columnStart[iColumn]+columnLength[iColumn];j++) {
         if (element[j]<0.0)
           good=false;
+      }
+    }
+    if (!good)
+      setWhen(0); // switch off
+  }
+}
+// Default Constructor
+CbcGreedyEquality::CbcGreedyEquality() 
+  :CbcHeuristic()
+{
+  // matrix  will automatically be empty
+  fraction_=1.0; // no branch and bound
+  originalNumberRows_=0;
+  algorithm_=0;
+  numberTimes_=100;
+}
+
+// Constructor from model
+CbcGreedyEquality::CbcGreedyEquality(CbcModel & model)
+  :CbcHeuristic(model)
+{
+  // Get a copy of original matrix
+  assert(model.solver());
+  matrix_ = *model.solver()->getMatrixByCol();
+  fraction_=1.0; // no branch and bound
+  originalNumberRows_=model.solver()->getNumRows();
+  algorithm_=0;
+  numberTimes_=100;
+}
+
+// Destructor 
+CbcGreedyEquality::~CbcGreedyEquality ()
+{
+}
+
+// Clone
+CbcHeuristic *
+CbcGreedyEquality::clone() const
+{
+  return new CbcGreedyEquality(*this);
+}
+
+// Copy constructor 
+CbcGreedyEquality::CbcGreedyEquality(const CbcGreedyEquality & rhs)
+:
+  CbcHeuristic(rhs),
+  matrix_(rhs.matrix_),
+  fraction_(rhs.fraction_),
+  originalNumberRows_(rhs.originalNumberRows_),
+  algorithm_(rhs.algorithm_),
+  numberTimes_(rhs.numberTimes_)
+{
+  this->setWhen(rhs.when());
+}
+
+// Assignment operator 
+CbcGreedyEquality & 
+CbcGreedyEquality::operator=( const CbcGreedyEquality& rhs)
+{
+  if (this != &rhs) {
+    setWhen(rhs.when());
+    matrix_=rhs.matrix_;
+    fraction_ = rhs.fraction_;
+    originalNumberRows_=rhs.originalNumberRows_;
+    algorithm_=rhs.algorithm_;
+    numberTimes_=rhs.numberTimes_;
+  }
+  return *this;
+}
+// Returns 1 if solution, 0 if not
+int
+CbcGreedyEquality::solution(double & solutionValue,
+			 double * betterSolution)
+{
+  if (!model_)
+    return 0;
+  // See if to do
+  if (!when()||(when()==1&&model_->phase()!=1))
+    return 0; // switched off
+  if (model_->getNodeCount()>numberTimes_)
+    return 0;
+  // See if at root node
+  bool atRoot = model_->getNodeCount()==0;
+  int passNumber = model_->getCurrentPassNumber();
+  if (atRoot&&passNumber!=1)
+    return 0;
+  OsiSolverInterface * solver = model_->solver();
+  const double * columnLower = solver->getColLower();
+  const double * columnUpper = solver->getColUpper();
+  // And original upper bounds in case we want to use them
+  const double * originalUpper = model_->continuousSolver()->getColUpper();
+  // But not if algorithm says so
+  if ((algorithm_%10)==0)
+    originalUpper = columnUpper;
+  const double * rowLower = solver->getRowLower();
+  const double * rowUpper = solver->getRowUpper();
+  const double * solution = solver->getColSolution();
+  const double * objective = solver->getObjCoefficients();
+  double integerTolerance = model_->getDblParam(CbcModel::CbcIntegerTolerance);
+  double primalTolerance;
+  solver->getDblParam(OsiPrimalTolerance,primalTolerance);
+
+  // This is number of rows when matrix was passed in
+  int numberRows = originalNumberRows_;
+  if (!numberRows)
+    return 0; // switched off
+
+  assert (numberRows==matrix_.getNumRows());
+  int iRow, iColumn;
+  double direction = solver->getObjSense();
+  double offset;
+  solver->getDblParam(OsiObjOffset,offset);
+  double newSolutionValue = -offset;
+  int returnCode = 0;
+
+  // Column copy
+  const double * element = matrix_.getElements();
+  const int * row = matrix_.getIndices();
+  const CoinBigIndex * columnStart = matrix_.getVectorStarts();
+  const int * columnLength = matrix_.getVectorLengths();
+
+  // Get solution array for heuristic solution
+  int numberColumns = solver->getNumCols();
+  double * newSolution = new double [numberColumns];
+  double * rowActivity = new double[numberRows];
+  memset(rowActivity,0,numberRows*sizeof(double));
+  double rhsNeeded=0;
+  for (iRow=0;iRow<numberRows;iRow++) 
+    rhsNeeded += rowUpper[iRow];
+  rhsNeeded *= fraction_;
+  bool allOnes=true;
+  // Get rounded down solution 
+  for (iColumn=0;iColumn<numberColumns;iColumn++) {
+    CoinBigIndex j;
+    double value = solution[iColumn];
+    if (solver->isInteger(iColumn)) {
+      // Round down integer
+      if (fabs(floor(value+0.5)-value)<integerTolerance) {
+        value=floor(CoinMax(value+1.0e-3,columnLower[iColumn]));
+      } else {
+        value=CoinMax(floor(value),columnLower[iColumn]);
+      }
+    }
+    // make sure clean
+    value = CoinMin(value,columnUpper[iColumn]);
+    value = CoinMax(value,columnLower[iColumn]);
+    newSolution[iColumn]=value;
+    double cost = direction * objective[iColumn];
+    newSolutionValue += value*cost;
+    for (j=columnStart[iColumn];
+         j<columnStart[iColumn]+columnLength[iColumn];j++) {
+      int iRow=row[j];
+      rowActivity[iRow] += value*element[j];
+      rhsNeeded -= value*element[j];
+      if (element[j]!=1.0)
+        allOnes=false;
+    }
+  }
+  // See if we round up
+  bool roundup = ((algorithm_%100)!=0);
+  if (roundup&&allOnes) {
+    // Get rounded up solution 
+    for (iColumn=0;iColumn<numberColumns;iColumn++) {
+      CoinBigIndex j;
+      double value = solution[iColumn];
+      if (solver->isInteger(iColumn)) {
+        // but round up if no activity
+        if (roundup&&value>=0.6&&!newSolution[iColumn]) {
+          bool choose=true;
+          for (j=columnStart[iColumn];
+               j<columnStart[iColumn]+columnLength[iColumn];j++) {
+            int iRow=row[j];
+            if (rowActivity[iRow]) {
+              choose=false;
+              break;
+            }
+          }
+          if (choose) {
+            newSolution[iColumn]=1.0;
+            double cost = direction * objective[iColumn];
+            newSolutionValue += cost;
+            for (j=columnStart[iColumn];
+                 j<columnStart[iColumn]+columnLength[iColumn];j++) {
+              int iRow=row[j];
+              rowActivity[iRow] += 1.0;
+              rhsNeeded -= 1.0;
+            }
+          }
+        }
+      }
+    }
+  }
+  // Get initial list
+  int * which = new int [numberColumns];
+  for (iColumn=0;iColumn<numberColumns;iColumn++) 
+    which[iColumn]=iColumn;
+  int numberLook=numberColumns;
+  // See if we want to perturb more
+  double perturb = ((algorithm_%10)==0) ? 0.1 : 0.25;
+  // Keep going round until a solution
+  while (true) {
+    // Get column with best ratio
+    int bestColumn=-1;
+    double bestRatio=COIN_DBL_MAX;
+    double bestStepSize = 0.0;
+    int newNumber=0;
+    for (int jColumn=0;jColumn<numberLook;jColumn++) {
+      int iColumn = which[jColumn];
+      CoinBigIndex j;
+      double value = newSolution[iColumn];
+      double cost = direction * objective[iColumn];
+      if (solver->isInteger(iColumn)) {
+        // use current upper or original upper
+        if (value+0.9999<originalUpper[iColumn]) {
+          double movement = 1.0;
+          double sum=0.0;
+          for (j=columnStart[iColumn];
+               j<columnStart[iColumn]+columnLength[iColumn];j++) {
+            int iRow=row[j];
+            double gap = rowUpper[iRow]-rowActivity[iRow];
+            double elementValue = allOnes ? 1.0 : element[j];
+            sum += elementValue;
+            if (movement*elementValue>gap) {
+              movement = gap/elementValue;
+            }
+	  }
+	  if (movement>0.999999) {
+            // add to next time
+            which[newNumber++]=iColumn;
+	    double ratio = (cost/sum)*(1.0+perturb*CoinDrand48());
+            // If at root
+            if (atRoot) {
+              if (fraction_==1.0)
+                ratio = iColumn; // choose first
+              else
+                ratio = - solution[iColumn]; // choose largest
+            }
+	    if (ratio<bestRatio) {
+              bestRatio=ratio;
+              bestColumn=iColumn;
+              bestStepSize=1.0;
+            }
+	  }
+	}
+      } else {
+        // continuous
+        if (value<columnUpper[iColumn]) {
+          double movement=1.0e50;
+          double sum=0.0;
+          for (j=columnStart[iColumn];
+               j<columnStart[iColumn]+columnLength[iColumn];j++) {
+            int iRow=row[j];
+            if (element[j]*movement+rowActivity[iRow]>rowUpper[iRow]) {
+	      movement = (rowUpper[iRow]-rowActivity[iRow])/element[j];;
+	    }
+            sum += element[j];
+	  }
+          // now ratio
+          if (movement>1.0e-7) {
+            // add to next time
+            which[newNumber++]=iColumn;
+            double ratio = (cost/sum)*(1.0+perturb*CoinDrand48());
+            if (ratio<bestRatio) {
+              bestRatio=ratio;
+              bestColumn=iColumn;
+              bestStepSize=movement;
+            }
+          }
+        }
+      }
+    }
+    if (bestColumn<0)
+      break; // we have finished
+    // Increase chosen column
+    newSolution[bestColumn] += bestStepSize;
+    double cost = direction * objective[bestColumn];
+    newSolutionValue += bestStepSize*cost;
+    for (CoinBigIndex j=columnStart[bestColumn];
+         j<columnStart[bestColumn]+columnLength[bestColumn];j++) {
+      int iRow = row[j];
+      rowActivity[iRow] += bestStepSize*element[j];
+      rhsNeeded -= bestStepSize*element[j];
+    }
+    if (rhsNeeded<1.0e-8)
+      break;
+  }
+  delete [] which;
+  if (fraction_<1.0&&rhsNeeded<1.0e-8&&newSolutionValue<solutionValue) {
+    // do branch and cut
+    // fix all nonzero
+    OsiSolverInterface * newSolver = model_->continuousSolver()->clone();
+    for (iColumn=0;iColumn<numberColumns;iColumn++) {
+      if (newSolver->isInteger(iColumn))
+        newSolver->setColLower(iColumn,newSolution[iColumn]);
+    }
+    // Reduce printout
+    newSolver->setHintParam(OsiDoReducePrint,true,OsiHintTry);
+    newSolver->setHintParam(OsiDoPresolveInInitial,true,OsiHintTry);
+    newSolver->setDblParam(OsiDualObjectiveLimit,solutionValue);
+    newSolver->initialSolve();
+    if (newSolver->isProvenOptimal()) {
+      CglPreProcess process;
+      /* Do not try and produce equality cliques and
+         do up to 5 passes */
+      OsiSolverInterface * solver2= process.preProcess(*newSolver);
+      if (!solver2) {
+        printf("Pre-processing says infeasible\n");
+        rhsNeeded=1.0; // so will be infeasible
+      } else {
+        solver2->resolve();
+        CbcModel model(*solver2);
+        model.setCutoff(solutionValue);
+        model.setMaximumNodes(200);
+        model.solver()->setHintParam(OsiDoReducePrint,true,OsiHintTry);
+        CbcStrategyDefault strategy(true,5,5,0);
+        model.setStrategy(strategy);
+        // Lightweight
+        model.setNumberStrong(5);
+        model.setNumberBeforeTrust(1);
+        model.solver()->setIntParam(OsiMaxNumIterationHotStart,10);
+        // Do complete search
+        model.branchAndBound();
+        if (model.getMinimizationObjValue()<solutionValue) {
+          // solution
+          rhsNeeded=0.0;
+          // post process
+          process.postProcess(*model.solver());
+          // Solution now back in newSolver
+          memcpy(newSolution,newSolver->getColSolution(),
+                 numberColumns*sizeof(double));
+          newSolutionValue = model.getMinimizationObjValue();
+        } else {
+          // no good
+          rhsNeeded=1.0; // so will be infeasible
+        }
+      }
+    } else {
+      rhsNeeded=1.0;
+    }
+    delete newSolver;
+  }
+  if (newSolutionValue<solutionValue&&rhsNeeded<1.0e-8) {
+    // check feasible
+    memset(rowActivity,0,numberRows*sizeof(double));
+    for (iColumn=0;iColumn<numberColumns;iColumn++) {
+      CoinBigIndex j;
+      double value = newSolution[iColumn];
+      if (value) {
+	for (j=columnStart[iColumn];
+	     j<columnStart[iColumn]+columnLength[iColumn];j++) {
+	  int iRow=row[j];
+	  rowActivity[iRow] += value*element[j];
+	}
+      }
+    }
+    // check was approximately feasible
+    bool feasible=true;
+    for (iRow=0;iRow<numberRows;iRow++) {
+      if(rowActivity[iRow]<rowLower[iRow]) {
+	if (rowActivity[iRow]<rowLower[iRow]-10.0*primalTolerance)
+	  feasible = false;
+      }
+    }
+    if (feasible) {
+      // new solution
+      memcpy(betterSolution,newSolution,numberColumns*sizeof(double));
+      solutionValue = newSolutionValue;
+      printf("** Solution of %g found by greedy\n",newSolutionValue);
+      returnCode=1;
+    }
+  }
+  delete [] newSolution;
+  delete [] rowActivity;
+  if (atRoot&&fraction_==1.0) {
+    // try quick search
+    fraction_=0.3;
+    int newCode=this->solution(solutionValue,betterSolution);
+    if (newCode)
+      returnCode=1;
+    fraction_=1.0;
+  }
+  return returnCode;
+}
+// update model
+void CbcGreedyEquality::setModel(CbcModel * model)
+{
+#define SLOPPY
+#ifndef SLOPPY
+  model_ = model;
+  assert(model_->solver());
+  *this = CbcGreedyEquality(*model); 
+#else
+  if (model_&&model!=model_) {
+    model_ = model;
+    assert(model_->solver());
+    *this = CbcGreedyEquality(*model); 
+  }
+#endif
+  validate();
+}
+// Resets stuff if model changes
+void 
+CbcGreedyEquality::resetModel(CbcModel * model)
+{
+#ifndef SLOPPY
+  model_ = model;
+  assert(model_->solver());
+  *this = CbcGreedyEquality(*model); 
+#else
+  // switch off
+  model_ = NULL;
+  matrix_ = CoinPackedMatrix();
+#endif
+}
+// Validate model i.e. sets when_ to 0 if necessary (may be NULL)
+void 
+CbcGreedyEquality::validate() 
+{
+  if (model_&&when()<10) {
+    if (model_->numberIntegers()!=
+        model_->numberObjects())
+      setWhen(0);
+    // Only works if costs positive, coefficients positive and all rows E or L
+    // And if values are integer
+    OsiSolverInterface * solver = model_->solver();
+    const double * columnLower = solver->getColLower();
+    const double * rowUpper = solver->getRowUpper();
+    const double * rowLower = solver->getRowLower();
+    const double * objective = solver->getObjCoefficients();
+    double direction = solver->getObjSense();
+
+    int numberRows = solver->getNumRows();
+    // Column copy
+    const double * element = matrix_.getElements();
+    const CoinBigIndex * columnStart = matrix_.getVectorStarts();
+    const int * columnLength = matrix_.getVectorLengths();
+    bool good = true;
+    for (int iRow=0;iRow<numberRows;iRow++) {
+      if (rowUpper[iRow]>1.0e30)
+        good = false;
+      if (rowLower[iRow]>0.0&&rowLower[iRow]!=rowUpper[iRow])
+        good = false;
+      if (floor(rowUpper[iRow]+0.5)!=rowUpper[iRow])
+        good = false;
+    }
+    int numberColumns = solver->getNumCols();
+    for (int iColumn=0;iColumn<numberColumns;iColumn++) {
+      if (objective[iColumn]*direction<0.0)
+        good=false;
+      if (columnLower[iColumn]<0.0)
+        good=false;
+      CoinBigIndex j;
+      for (j=columnStart[iColumn];
+           j<columnStart[iColumn]+columnLength[iColumn];j++) {
+        if (element[j]<0.0)
+          good=false;
+        if (floor(element[j]+0.5)!=element[j])
+          good = false;
       }
     }
     if (!good)
--- Samples/CbcHeuristicGreedy.hpp
+++ Samples/CbcHeuristicGreedy.hpp
@@ -1,10 +1,10 @@
-// Copyright (C) 2002, International Business Machines
+// Copyright (C) 2005, International Business Machines
 // Corporation and others.  All Rights Reserved.
 #ifndef CbcHeuristicGreedy_H
 #define CbcHeuristicGreedy_H
 
 #include "CbcHeuristic.hpp"
-/** Greedy heuristic class
+/** Greedy heuristic classes
  */
 
 class CbcGreedyCover : public CbcHeuristic {
@@ -56,6 +56,11 @@ public:
   { return algorithm_; };
   inline void setAlgorithm(int value)
   { algorithm_=value; };
+  // Only do this many times
+  inline int numberTimes() const
+  { return numberTimes_; };
+  inline void setNumberTimes(int value)
+  { numberTimes_=value; };
 
 protected:
   // Data
@@ -70,10 +75,95 @@ protected:
      If 10 added perturb ratios more
   */
   int algorithm_;
+  /// Do this many times
+  int numberTimes_;
 
 private:
   /// Illegal Assignment operator 
   CbcGreedyCover & operator=(const CbcGreedyCover& rhs);
+};
+
+
+class CbcGreedyEquality : public CbcHeuristic {
+public:
+
+  // Default Constructor 
+  CbcGreedyEquality ();
+
+  /* Constructor with model - assumed before cuts
+     Initial version does not do Lps
+  */
+  CbcGreedyEquality (CbcModel & model);
+  
+  // Copy constructor 
+  CbcGreedyEquality ( const CbcGreedyEquality &);
+   
+  // Destructor 
+  ~CbcGreedyEquality ();
+  
+  /// Clone
+  virtual CbcHeuristic * clone() const;
+
+  /// update model (This is needed if cliques update matrix etc)
+  virtual void setModel(CbcModel * model);
+  
+  /** returns 0 if no solution, 1 if valid solution.
+      Sets solution values if good, sets objective value (only if good)
+      We leave all variables which are at one at this node of the
+      tree to that value and will
+      initially set all others to zero.  We then sort all variables in order of their cost
+      divided by the number of entries in rows which are not yet covered.  We randomize that
+      value a bit so that ties will be broken in different ways on different runs of the heuristic.
+      We then choose the best one and set it to one and repeat the exercise.  
+
+  */
+  virtual int solution(double & objectiveValue,
+		       double * newSolution);
+  /// Validate model i.e. sets when_ to 0 if necessary (may be NULL)
+  virtual void validate() ;
+  /// Resets stuff if model changes
+  virtual void resetModel(CbcModel * model);
+  /* Algorithm
+     0 - use current upper bounds
+     1 - use original upper bounds
+     If 10 added perturb ratios more
+     if 100 added round up all >=0.5
+  */
+  inline int algorithm() const
+  { return algorithm_; };
+  inline void setAlgorithm(int value)
+  { algorithm_=value; };
+  // Fraction of rhs to cover before branch and cut
+  inline void setFraction(double value)
+  { fraction_ = value;};
+  inline double fraction() const
+  { return fraction_;};
+  // Only do this many times
+  inline int numberTimes() const
+  { return numberTimes_; };
+  inline void setNumberTimes(int value)
+  { numberTimes_=value; };
+protected:
+  // Data
+
+  // Original matrix by column
+  CoinPackedMatrix matrix_;
+  // Fraction of rhs to cover before branch and cut
+  double fraction_;
+  // original number of rows
+  int originalNumberRows_;
+  /* Algorithm
+     0 - use current upper bounds
+     1 - use original upper bounds
+     If 10 added perturb ratios more
+  */
+  int algorithm_;
+  /// Do this many times
+  int numberTimes_;
+
+private:
+  /// Illegal Assignment operator 
+  CbcGreedyEquality & operator=(const CbcGreedyEquality& rhs);
 };
 
 

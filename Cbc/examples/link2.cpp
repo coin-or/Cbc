8025bbe5c3b8fe734fc405fe57742687c80ae26b
John Forrest <john.forrest@fastercoin.com>
2007-07-11 21:05:34 +0000

    updated example

--- Cbc/examples/link2.cpp
+++ Cbc/examples/link2.cpp
@@ -13,6 +13,7 @@
 #include "OsiSolverInterface.hpp"
 #include "CbcModel.hpp"
 #include "CbcBranchActual.hpp"
+#include "CbcCutGenerator.hpp"
 #include "OsiChooseVariable.hpp"
 #include "CoinModel.hpp"
 // For Linked Ordered Sets
@@ -28,7 +29,7 @@
 This shows how to get a global solution to a non-convex quadratically 
 constrained problem.
 
-Having variables integer would make easier
+Having variables integer would probably make easier
 ************************************************************************/
 
 int main (int argc, const char *argv[])
@@ -38,47 +39,93 @@ int main (int argc, const char *argv[])
   // Create model 
   CoinModel build;
   // size
-  int numberX=5;
-  int numberConstraints=20;
+  int numberX=20;
+  int numberConstraints=1000;
   // variables are x sub j and w sub ij which is x sub j * x sub i
   char name[20];
   char product[20];
   int i;
   // pointers to W (-1 if not existing)
   int ** whichW = new int * [numberX];
-  int numberW=0;
+  for (i=0;i<numberX;i++) {
+    int * sequenceW = new int[numberX];
+    int j;
+    for (j=0;j<numberX;j++)
+      sequenceW[j]=-1;
+    whichW[i]=sequenceW;
+  }
+  /* now decide on W.
+     When both x and y are continuous then the default method for x*y will branch on x
+     until it is fixed and x*y can be modeled exactly.  In this case it is better not to
+     use a triangular version for W but to randomize so each variable is the "x" one
+     about the same number of times.
+  */
+  int neededW=(numberX*(numberX+1))/2;
+  int * wI = new int[neededW];
+  int * wJ = new int[neededW];
+  while (neededW) {
+    bool gotColumn=false;
+    int iColumn=-1;
+    int jColumn=-1;
+    while(!gotColumn) {
+      iColumn=(int) floor(CoinDrand48()*numberX);
+      jColumn=(int) floor(CoinDrand48()*numberX);
+      if (iColumn==numberX||jColumn==numberX)
+	continue;
+      int * sequenceW = whichW[iColumn];
+      if (sequenceW[jColumn]==-1) {
+	// available
+	gotColumn=true;
+	break;
+      }
+    }
+    int * sequenceW = whichW[iColumn];
+    sequenceW[jColumn]=1;
+    neededW--;
+    if (iColumn!=jColumn) {
+      int * sequenceW = whichW[jColumn];
+      sequenceW[iColumn]=-2;
+    }
+  }
   // build columns
   for (i=0;i<numberX;i++) {
     sprintf(name,"X_%d",i);
+    // If we know tighter bounds then better
     build.addColumn(0,NULL,NULL,0.0,1.0,0.0,name);
   }
-  // now W
+  // now do W
+  int numberW=0;
   for (i=0;i<numberX;i++) {
-    // and do pointers to W
-    int * sequenceW = new int[numberX];
-    int j;
-    for (j=0;j<i;j++)
-      sequenceW[j]=-1;
-    for (;j<numberX;j++) {
-      sequenceW[j]=numberX+numberW;
-      assert (sequenceW[j]==build.numberColumns());
-      sprintf(name,"W_%d_%d",i,j);
-      // minimize sums of squares
-      double value = (i==j) ? 1.0 : 0.0;
-      build.addColumn(0,NULL,NULL,0.0,1.0,value,name);
-      numberW++;
+    int * sequenceW = whichW[i];
+    for (int j=0;j<numberX;j++) {
+      if (sequenceW[j]>0) {
+	sequenceW[j]=numberX+numberW;
+	assert (sequenceW[j]==build.numberColumns());
+	sprintf(name,"W_%d_%d",i,j);
+	// minimize sums of squares of x so ..
+	double value = (i==j) ? 1.0 : 0.0;
+	build.addColumn(0,NULL,NULL,0.0,1.0,value,name);
+	wI[numberW]=i;
+	wJ[numberW]=j;
+	numberW++;
+      }
     }
-    whichW[i]=sequenceW;
   }
   int numberColumns = build.numberColumns();
   int * column = new int [numberColumns];
   double * element = new double [numberColumns];
-  // first row says sum of x >= 1.0
+  // first row says sum of x >= 1.0 *** no make equality?
   for (i=0;i<numberX;i++) {
     column[i]=i;
     element[i]=1.0;
   }
+  // Should be stronger if we make sum x == 1 rather >= 1
+#define EQUALITY
+#ifndef EQUALITY
   build.addRow(numberX,column,element,1.0,COIN_DBL_MAX);
+#else
+  build.addRow(numberX,column,element,1.0,1.0);
+#endif
   // Now define W
   for (i=0;i<numberX;i++) {
     int * sequenceW = whichW[i];
@@ -97,7 +144,9 @@ int main (int argc, const char *argv[])
     }
   }
 #if 1
-  // Optional linear constraints 
+  /* Optional linear constraints 
+     These are theoretically redundant but may tighten relaxation
+  */
   for (i=0;i<numberX;i++) {
     // We want to add in sum over j W_i_j >= X_i
     int * sequenceW = whichW[i];
@@ -116,11 +165,19 @@ int main (int argc, const char *argv[])
     // add in X part of row
     column[n]=i;
     element[n++]=-1.0;
+#ifndef EQUALITY
     build.addRow(n,column,element,0.0,COIN_DBL_MAX);
+#else
+    build.addRow(n,column,element,0.0,0.0);
+#endif
   }
 #endif
   // Now add in random quadratics - no linear term but that would be easy (X variables)
-  double scaleFactor=0.5;
+  double rhs=0.5; // So constraints do something
+  if (numberX==8)
+    rhs=0.366;
+  else if (numberX==20)
+    rhs=0.25; // so will be quickish!
 #define USE_CUTS
 #ifndef USE_CUTS
   // Put constraints in matrix
@@ -135,7 +192,10 @@ int main (int argc, const char *argv[])
 	element[n++]=value;
       }
     }
-    build.addRow(n,column,element,-COIN_DBL_MAX,scaleFactor);
+    // make sure last one feasible at 1.0
+    if (element[n-1]>rhs)
+      element[n-1]=0.9*rhs;
+    build.addRow(n,column,element,-COIN_DBL_MAX,rhs);
   }
 #else
   // Put constraints in as cuts
@@ -154,8 +214,11 @@ int main (int argc, const char *argv[])
 	element[n++]=value;
       }
     }
-    storedCuts.addCut(-COIN_DBL_MAX,scaleFactor,n,column,element);
-    build2.addRow(n,column,element,-COIN_DBL_MAX,scaleFactor);
+    // make sure last one feasible at 1.0
+    if (element[n-1]>rhs)
+      element[n-1]=0.9*rhs;
+    storedCuts.addCut(-COIN_DBL_MAX,rhs,n,column,element);
+    build2.addRow(n,column,element,-COIN_DBL_MAX,rhs);
   }
   // load from coin model
   OsiSolverLink solver3;
@@ -173,19 +236,16 @@ int main (int argc, const char *argv[])
 
   // load from coin model
   OsiSolverLink solver1;
-  //OsiSolverInterface * solver2 = solver1.clone();
-  ///CbcModel model;
-  //model.assignSolver(solver2,true);
-  //OsiSolverLink * si =
-  //dynamic_cast<OsiSolverLink *>(model.solver()) ;
-  //assert (si != NULL);
-  //solver1.setSpecialOptions2(16);
   solver1.setDefaultMeshSize(0.001);
-  // need some relative granularity
+  // Use coarse grid so we can solve quickly
   solver1.setDefaultMeshSize(0.01);
+  solver1.setDefaultMeshSize(0.001);
+  //solver1.setDefaultMeshSize(0.0001);
   solver1.setIntegerPriority(1000);
   solver1.setBiLinearPriority(10000);
   solver1.load(build,true,2);
+  // Add more objects with even coarser grid
+  solver1.setBiLinearPriorities(10,0.2);
   CbcModel model(solver1);
   // set more stuff
   OsiSolverLink * clpSolver = dynamic_cast< OsiSolverLink*> (model.solver());
@@ -207,9 +267,12 @@ int main (int argc, const char *argv[])
   model.setBranchingMethod(decision);
   model.setLogLevel(1);
 #ifdef USE_CUTS
-  model.addCutGenerator(&storedCuts,1,"Stored cuts",false,true);
-  model.setMaximumCutPasses(4);
+  model.addCutGenerator(&storedCuts,1,"Stored cuts",true,false);
+  model.cutGenerator(0)->setMustCallAgain(true);;
+  //model.addCutGenerator(&storedCuts,1,"Stored cuts",false,true);
+  //model.setMaximumCutPasses(4);
 #endif
+  //model.setNumberThreads(4);
   model.branchAndBound();
 
   std::cout<<"took "<<CoinCpuTime()-time1<<" seconds, "
@@ -218,17 +281,70 @@ int main (int argc, const char *argv[])
 	   <<(!model.status() ? " Finished" : " Not finished")
 	   <<std::endl;
 
+  int numberGenerators = model.numberCutGenerators();
+  for (int iGenerator=0;iGenerator<numberGenerators;iGenerator++) {
+    CbcCutGenerator * generator = model.cutGenerator(iGenerator);
+    std::cout<<generator->cutGeneratorName()<<" was tried "
+	     <<generator->numberTimesEntered()<<" times and created "
+	     <<generator->numberCutsInTotal()<<" cuts of which "
+	     <<generator->numberCutsActive()<<" were active after adding rounds of cuts";
+    if (generator->timing())
+      std::cout<<" ( "<<generator->timeInCutGenerator()<<" seconds)"<<std::endl;
+    else
+      std::cout<<std::endl;
+  }
 
   if (model.getMinimizationObjValue()<1.0e50) {
     
     const double * solution = model.bestSolution();
-    for (i=0;i<numberColumns;i++)
-      printf("%d %g\n",i,solution[i]);
+    double obj=0.0;
+    for (i=0;i<numberX;i++) {
+      printf("%d %s %g\n",i,build.columnName(i),solution[i]);
+      obj += solution[i]*solution[i];
+    }
+    printf("true objective %g\n",obj);
+    for (;i<numberColumns;i++) {
+      int iX = wI[i-numberX];
+      int jX = wJ[i-numberX];
+      printf("%d %s %g - should be %g\n",i,build.columnName(i),solution[i],solution[iX]*solution[jX]);
+    }
+#ifdef USE_CUTS
+    double worstViolation=0.0;
+    double sumViolation=0.0;
+    for (i=0;i<numberConstraints;i++) {
+      const OsiRowCut * cut = storedCuts.rowCutPointer(i);
+      const CoinPackedVector row = cut->row();
+      int n = row.getNumElements();
+      const int * column = row.getIndices();
+      const double * element = row.getElements();
+      double sum=0.0;
+      double sumApprox=0.0;
+      for (int j=0;j<n;j++) {
+	int iColumn = column[j];
+	sumApprox += solution[iColumn]*element[j];
+	int iX = wI[iColumn-numberX];
+	int jX = wJ[iColumn-numberX];
+	sum += solution[iX]*solution[jX]*element[j];
+      }
+      if (sum>cut->ub()+1.0e-6) 
+	printf("*** %d %g (approx %g) > %g\n",i,sum,sumApprox,cut->ub());
+      else if (sum>cut->ub()-1.0e-3) 
+	printf("    %d %g (approx %g) =~ %g\n",i,sum,sumApprox,cut->ub());
+      if (sum>cut->ub()) {
+	double violation = sum-cut->ub();
+	sumViolation += violation;
+	worstViolation = CoinMax(worstViolation,violation);
+      }
+    }
+    printf("Total violation %g, worst was %g\n",sumViolation,worstViolation);
+#endif
   }
   // deletes
   for (i=0;i<numberX;i++) {
     delete [] whichW[i];
   }
   delete [] whichW;
+  delete [] wI;
+  delete [] wJ;
   return 0;
 }    

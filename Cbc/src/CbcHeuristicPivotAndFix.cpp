b3eb73e5caf60ae88ec53923eb574c347ba242de
John Forrest <john.forrest@fastercoin.com>
2008-09-05 14:31:05 +0000

    adding dummy heuristic

---
+++ Cbc/src/CbcHeuristicPivotAndFix.cpp
@@ -0,0 +1,93 @@
+// Copyright (C) 2008, International Business Machines
+// Corporation and others.  All Rights Reserved.
+#if defined(_MSC_VER)
+// Turn off compiler warning about long names
+#  pragma warning(disable:4786)
+#endif
+#include <cassert>
+#include <cstdlib>
+#include <cmath>
+#include <cfloat>
+
+#include "OsiSolverInterface.hpp"
+#include "CbcModel.hpp"
+#include "CbcMessage.hpp"
+#include "CbcHeuristicPivotAndFix.hpp"
+
+// Default Constructor
+CbcHeuristicPivotAndFix::CbcHeuristicPivotAndFix() 
+  :CbcHeuristic()
+{
+}
+
+// Constructor with model - assumed before cuts
+
+CbcHeuristicPivotAndFix::CbcHeuristicPivotAndFix(CbcModel & model)
+  :CbcHeuristic(model)
+{
+}
+
+// Destructor 
+CbcHeuristicPivotAndFix::~CbcHeuristicPivotAndFix ()
+{
+}
+
+// Clone
+CbcHeuristic *
+CbcHeuristicPivotAndFix::clone() const
+{
+  return new CbcHeuristicPivotAndFix(*this);
+}
+// Create C++ lines to get to current state
+void 
+CbcHeuristicPivotAndFix::generateCpp( FILE * fp) 
+{
+  CbcHeuristicPivotAndFix other;
+  fprintf(fp,"0#include \"CbcHeuristicPivotAndFix.hpp\"\n");
+  fprintf(fp,"3  CbcHeuristicPivotAndFix heuristicPFX(*cbcModel);\n");
+  CbcHeuristic::generateCpp(fp,"heuristicPFX");
+  fprintf(fp,"3  cbcModel->addHeuristic(&heuristicPFX);\n");
+}
+
+// Copy constructor 
+CbcHeuristicPivotAndFix::CbcHeuristicPivotAndFix(const CbcHeuristicPivotAndFix & rhs)
+:
+  CbcHeuristic(rhs)
+{
+}
+
+// Assignment operator 
+CbcHeuristicPivotAndFix & 
+CbcHeuristicPivotAndFix::operator=( const CbcHeuristicPivotAndFix& rhs)
+{
+  if (this!=&rhs) {
+    CbcHeuristic::operator=(rhs);
+  }
+  return *this;
+}
+
+// Resets stuff if model changes
+void 
+CbcHeuristicPivotAndFix::resetModel(CbcModel * model)
+{
+  //CbcHeuristic::resetModel(model);
+}
+/*
+  Comments needed
+  Returns 1 if solution, 0 if not */
+int
+CbcHeuristicPivotAndFix::solution(double & solutionValue,
+			 double * betterSolution)
+{
+
+  numCouldRun_++;
+  int returnCode = 0;
+  return returnCode;
+}
+// update model
+void CbcHeuristicPivotAndFix::setModel(CbcModel * model)
+{
+  // probably same as resetModel
+}
+
+  
---
+++ Cbc/src/CbcHeuristicPivotAndFix.hpp
@@ -0,0 +1,54 @@
+// Copyright (C) 2008, International Business Machines
+// Corporation and others.  All Rights Reserved.
+#ifndef CbcHeuristicPivotAndFix_H
+#define CbcHeuristicPivotAndFix_H
+
+#include "CbcHeuristic.hpp"
+/** LocalSearch class
+ */
+
+class CbcHeuristicPivotAndFix : public CbcHeuristic {
+public:
+
+  // Default Constructor 
+  CbcHeuristicPivotAndFix ();
+
+  /* Constructor with model - assumed before cuts
+     Initial version does not do Lps
+  */
+  CbcHeuristicPivotAndFix (CbcModel & model);
+  
+  // Copy constructor 
+  CbcHeuristicPivotAndFix ( const CbcHeuristicPivotAndFix &);
+   
+  // Destructor 
+  ~CbcHeuristicPivotAndFix ();
+  
+  /// Clone
+  virtual CbcHeuristic * clone() const;
+
+  /// Assignment operator 
+  CbcHeuristicPivotAndFix & operator=(const CbcHeuristicPivotAndFix& rhs);
+
+  /// Create C++ lines to get to current state
+  virtual void generateCpp( FILE * fp) ;
+
+  /// Resets stuff if model changes
+  virtual void resetModel(CbcModel * model);
+
+  /// update model (This is needed if cliques update matrix etc)
+  virtual void setModel(CbcModel * model);
+  
+  using CbcHeuristic::solution ;
+  /** returns 0 if no solution, 1 if valid solution.
+      Sets solution values if good, sets objective value (only if good)
+      needs comments
+  */
+  virtual int solution(double & objectiveValue,
+		       double * newSolution);
+
+protected:
+};
+
+
+#endif

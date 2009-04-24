d73ee9879c0ff24fde304b899d7d0035e288427b
John Forrest <john.forrest@fastercoin.com>
2009-04-24 11:13:50 +0000

    update example

--- Cbc/examples/fpump.cpp
+++ Cbc/examples/fpump.cpp
@@ -4,6 +4,9 @@
 #include "CbcModel.hpp"
 #include "CbcHeuristicFPump.hpp"
 #include "CbcHeuristicRINS.hpp"
+#include "CbcHeuristicLocal.hpp"
+#include "CbcHeuristicDiveCoefficient.hpp"
+#include "CbcHeuristicDiveFractional.hpp"
 
 // Using as solver
 #include "OsiClpSolverInterface.hpp"
@@ -29,8 +32,20 @@ int main (int argc, const char *argv[])
   // uncomment to reduce printout
   //model.setLogLevel(1);
   //model.solver()->setHintParam(OsiDoReducePrint,true,OsiHintTry);
+  CbcHeuristicDiveCoefficient heuristicDC(model);
+  heuristicDC.setHeuristicName("DiveCoefficient");
+  // allow to exit if close enough to optimum
+  heuristicDC.setSwitches(1);
+  model.addHeuristic(&heuristicDC);
+  CbcHeuristicDiveFractional heuristicDF(model);
+  heuristicDF.setHeuristicName("DiveFractional");
+  heuristicDF.setSwitches(1);
+  model.addHeuristic(&heuristicDF);
   CbcHeuristicFPump pump(model);
-  //pump.setMaximumTime(60);
+  // allow to exit if close enough to optimum
+  // plus some dubious options
+  pump.setSwitches(1+4+8);
+  pump.setMaximumTime(200);
   pump.setMaximumPasses(100);
   pump.setMaximumRetries(1); 
   pump.setFixOnReducedCosts(1);
@@ -38,6 +53,7 @@ int main (int argc, const char *argv[])
   pump.setFractionSmall(1.0);
   pump.setWhen(13);
   pump.setFakeCutoff(objValue+0.01*fabs(objValue));
+  pump.setReducedCostMultiplier(0.1);
   pump.setFeasibilityPumpOptions(80);
   model.addHeuristic(&pump);
   pump.setFakeCutoff(objValue+0.05*fabs(objValue));
@@ -57,9 +73,16 @@ int main (int argc, const char *argv[])
   rins.setFractionSmall(0.5);
   rins.setDecayFactor(5.0);
   model.addHeuristic(&rins) ;
+  CbcHeuristicLocal local(model);
+  local.setHeuristicName("LOCAL");
+  local.setFractionSmall(0.5);
+  local.setSearchType(1);
+  model.addHeuristic(&local) ;
   model.setThreadMode(8);
   model.setNumberThreads(2);
   model.setMaximumNodes(-1);
+  // Say what is good enough for solution
+  model.setAllowableFractionGap(0.005);
   // Do root
   model.branchAndBound();
 #if 0

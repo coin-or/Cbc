1b825796887702e951f90e1c2f7dd33b0e74fb8a
Bjarni Kristjansson <bjarni@maximalsoftware.com>
2009-11-05 23:21:49 +0000

    Test commit from Bjarni

--- Cbc/src/Cbc_ampl.cpp
+++ Cbc/src/Cbc_ampl.cpp
@@ -23,6 +23,7 @@ IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
 ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 THIS SOFTWARE.
 ****************************************************************/
+Bjarni
 #ifdef COIN_HAS_ASL
 
 #include "CbcConfig.h"

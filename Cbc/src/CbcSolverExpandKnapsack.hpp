7c63e49305c4d65aca8043920b626d0d34ad4191
Bjarni Kristjansson <bjarni@maximalsoftware.com>
2009-12-11 04:46:08 +0000

    Extracted expandKnapsack() from CbcSolver.cpp and placed it in CbcSolverExpandKnapsack.cpp, updated libCbc.vcproj (v9) to include CbcSolverExpandKnapsack.cpp

---
+++ Cbc/src/CbcSolverExpandKnapsack.hpp
@@ -0,0 +1,20 @@
+/* $Id: CbcSolverExpandKnapsack.hpp 1212 2009-08-21 16:19:13Z forrest $ */
+// Copyright (C) 2007, International Business Machines
+// Corporation and others.  All Rights Reserved.
+
+/*! \file CbcSolverExpandKnapsack.hpp
+    \brief Expanding possibilities of x*y, where x*y are both integers, constructing
+  a knapsack constraint. Results in a tighter model.
+*/
+
+
+#ifndef CbcSolverExpandKnapsack_H
+#define CbcSolverExpandKnapsack_H
+
+OsiSolverInterface *
+expandKnapsack(CoinModel & model, int * whichColumn, int * knapsackStart,
+               int * knapsackRow, int &numberKnapsack,
+               CglStored & stored, int logLevel,
+               int fixedPriority, int SOSPriority, CoinModel & tightenedModel);
+
+#endif

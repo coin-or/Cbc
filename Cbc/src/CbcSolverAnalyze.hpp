acddde5e26737ac3c439678722d3b8b4c91201ac
Bjarni Kristjansson <bjarni@maximalsoftware.com>
2009-12-11 02:43:19 +0000

    Extracted analyze() from CbcSolver.cpp and placed it in CbcSolverAnalyze.cpp, updated libCbc.vcproj (v9) to include CbcSolverAnalyze.cpp

---
+++ Cbc/src/CbcSolverAnalyze.hpp
@@ -0,0 +1,19 @@
+/* $Id: CbcSolver.hpp 1212 2009-08-21 16:19:13Z forrest $ */
+// Copyright (C) 2007, International Business Machines
+// Corporation and others.  All Rights Reserved.
+
+/*! \file CbcSolverAnalyze.hpp
+    \brief Look to see if a constraint is all-integer (variables & coeffs), or could be
+  all integer.
+*/
+
+#ifndef CbcSolverAnalyze_H
+#define CbcSolverAnalyze_H
+
+
+int * analyze(OsiClpSolverInterface * solverMod, int & numberChanged,
+		     double & increment, bool changeInt,
+		     CoinMessageHandler * generalMessageHandler, bool noPrinting);
+		     
+#endif
+

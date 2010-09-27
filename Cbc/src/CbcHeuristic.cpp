a009b22ba5d87bba589bbf6459583b77f629623b
Lou Hafer <lou@sfu.ca>
2010-09-27 20:07:52 +0000

    An unsigned type wasn't the best choice here.

--- Cbc/src/CbcHeuristic.cpp
+++ Cbc/src/CbcHeuristic.cpp
@@ -43,7 +43,7 @@ CbcHeuristicNode::CbcHeuristicNode(const CbcHeuristicNode& rhs)
 void
 CbcHeuristicNodeList::gutsOfDelete()
 {
-    for (size_t i = nodes_.size() - 1; i >= 0; --i) {
+    for (int i = (static_cast<int>(nodes_.size())) - 1; i >= 0; --i) {
         delete nodes_[i];
     }
 }

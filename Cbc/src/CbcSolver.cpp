862ff672c9b01d9aaf0763ea2c2f44eff4030687
John Forrest <john.forrest@fastercoin.com>
2014-03-11 20:21:33 +0000

    fix segfault if infeasible

--- Cbc/src/CbcSolver.cpp
+++ Cbc/src/CbcSolver.cpp
@@ -8781,6 +8781,8 @@ clp watson.mps -\nscaling off\nprimalsimplex"
 				  lpSolver->initialSolve();
 				}
                                     // Write solution header (suggested by Luigi Poderico)
+				    // Refresh solver
+				    lpSolver = clpSolver->getModelPtr();
                                     lpSolver->computeObjectiveValue(false);
                                     double objValue = lpSolver->getObjValue();
                                     int iStat = lpSolver->status();

f6f52f9fb5ec77f3bc188221cf3a56b87689386a
Ted Ralphs <ted@lehigh.edu>
2015-02-09 05:16:29 +0000

    restoring Cbc/stable/2.9

--- Cbc/src/config_cbc_default.h
+++ Cbc/src/config_cbc_default.h
@@ -5,7 +5,7 @@
 /***************************************************************************/
 
 /* Version number of project */
-#define CBC_VERSION "2.9.2"
+#define CBC_VERSION "2.9"
 
 /* Major Version number of project */
 #define CBC_VERSION_MAJOR 2
@@ -14,4 +14,4 @@
 #define CBC_VERSION_MINOR 9
 
 /* Release Version number of project */
-#define CBC_VERSION_RELEASE 2
+#define CBC_VERSION_RELEASE 9999

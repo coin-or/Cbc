ee45fd5a16cc7f670b61f02e9901db5020250fd6
Haroldo Santos <haroldo.santos@gmail.com>
2016-01-19 17:31:41 +0000

    storing original columns info in CbcModel

--- Cbc/src/CbcSolver.cpp
+++ Cbc/src/CbcSolver.cpp
@@ -4179,8 +4179,7 @@ int CbcMain1 (int argc, const char *argv[],
 					}
                                         solver2 = process.preProcessNonDefault(*saveSolver, translate[preProcess], numberPasses,
                                                                                tunePreProcess);
-                                        /*solver2->writeMps("after");
-                                          saveSolver->writeMps("before");*/
+                                        model_.setOriginalColumns( process.originalColumns() );
                                         osiclp->getModelPtr()->setPerturbation(savePerturbation);
 					osiclp->getModelPtr()->setMoreSpecialOptions(saveOptions);
                                     }

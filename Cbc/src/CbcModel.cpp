474e630c6c645400fb9fbd64787a630d97348afa
John Forrest <john.forrest@fastercoin.com>
2016-02-12 10:02:43 +0000

    fix minor memory leak

--- Cbc/src/CbcModel.cpp
+++ Cbc/src/CbcModel.cpp
@@ -8627,6 +8627,7 @@ CbcModel::solveWithCuts (OsiCuts &cuts, int numberTries, CbcNode *node)
             if ( maximumSecondsReached() ) {
                 numberTries = -1000; // exit
 		feasible = false;
+		delete [] addCuts;
                 break;
             }
 #     ifdef CBC_DEBUG

d1eb14323535073ba41d8b279725727dc0ff6769
Ted Ralphs <ted@lehigh.edu>
2016-02-26 05:01:20 +0000

    temporarily committing release candidate

--- Cbc/src/config_cbc_default.h
+++ Cbc/src/config_cbc_default.h
@@ -5,7 +5,7 @@
 /***************************************************************************/
 
 /* Version number of project */
-#define CBC_VERSION "2.9"
+#define CBC_VERSION "2.9.8"
 
 /* Major Version number of project */
 #define CBC_VERSION_MAJOR 2
@@ -14,4 +14,4 @@
 #define CBC_VERSION_MINOR 9
 
 /* Release Version number of project */
-#define CBC_VERSION_RELEASE 9999
+#define CBC_VERSION_RELEASE 8

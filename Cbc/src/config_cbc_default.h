eb17d7c50cb5f6feef1e4f43af3bb2f1244c32af
Stefan Vigerske <svigerske@gams.com>
2019-06-14 22:05:04 +0000

    restoring Cbc/stable/2.10

--- Cbc/src/config_cbc_default.h
+++ Cbc/src/config_cbc_default.h
@@ -5,7 +5,7 @@
 /***************************************************************************/
 
 /* Version number of project */
-#define CBC_VERSION "2.10.3"
+#define CBC_VERSION "2.10"
 
 /* Major Version number of project */
 #define CBC_VERSION_MAJOR 2
@@ -14,7 +14,7 @@
 #define CBC_VERSION_MINOR 10
 
 /* Release Version number of project */
-#define CBC_VERSION_RELEASE 3
+#define CBC_VERSION_RELEASE 9999
 
 /* vi: softtabstop=2 shiftwidth=2 expandtab tabstop=2
 */

d3f2a54226aa7f4a19bd69c33620732f2a76ac78
Ted Ralphs <ted@lehigh.edu>
2020-04-30 19:58:46 +0000

    Fixing mistake in bintray.sh

--- .ci/bintray.sh
+++ .ci/bintray.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-while [[ ! `curl -utkralphs:$BINTRAY_API -X PUT -H "Content-Type: application/json" -d'{"list_in_downloads":true}' https://api.bintray.com/file_metadata/coin-or/download/blah.zip` =~ success ]]
+while [[ ! `curl -utkralphs:$BINTRAY_API -X PUT -H "Content-Type: application/json" -d'{"list_in_downloads":true}' https://api.bintray.com/file_metadata/coin-or/download/$APPVEYOR_PROJECT_NAME-$VERSION-$ARCH.zip` =~ success ]]
 do
     echo "Command failed... trying again in 10 seconds"
     #No sleep command in MSys bash

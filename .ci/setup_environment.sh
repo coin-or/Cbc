373356b01954694c3dcefc2e0cd970ab6f1f2248
Ted Ralphs <ted@lehigh.edu>
2020-06-01 17:45:47 -0400

    More minor tweaks

--- .ci/setup_environment.sh
+++ .ci/setup_environment.sh
@@ -5,36 +5,44 @@ case $CC in
         if [ $TRAVIS_OS_NAME = osx ]; then
             export CC=gcc-9
             export CXX=g++-9
-            CCVERSION=9
+            export CCVERSION=gcc9
         else
-            export CCVERSION=$($CC -dumpversion)
+            export CCVERSION=gcc$($CC -dumpversion)
         fi
         ;;
     clang)
-        export CCVERSION=$(clang --version | fgrep version | \
-                           sed "s/.*version \([0-9]*\.[0-9]*\).*/\1/")
+        export CCVERSION=clang$(clang --version | fgrep version | \
+                                sed "s/.*version \([0-9]*\.[0-9]*\).*/\1/" | \
+                                cut -d "." -f 1)
         ;;
 esac
 declare -a DBG_ARGS
+declare -a ADD_ARGS
+declare -a COMMON_ARGS
 export DBG_ARGS=()
+export ADD_ARGS=()
+export COMMON_ARGS=()
 if [ x$DEBUG = x"true" ]; then
-    export DBG_ARGS=( --enable-debug )
     export DBGN="-dbg"
-    export CXXFLAGS=( -Og -g)
+    export DBG_ARGS+=( --enable-debug )
+    export CXXFLAGS="-Og -g"
 fi
 if [ x$ASAN = x"true" ]; then
     export ASN="-asan"
     export CXXFLAGS="${CXXFLAGS} -fsanitize=address"
     export LDFLAGS="-lasan"
 fi
-declare -a ADD_ARGS
-export ADD_ARGS=()
 if [ x$BUILD_STATIC = x"true" ]; then
-    export STATIC=( -static )
-    export ADD_ARGS=( --static --with-lapack='-llapack -lblas -lgfortran -lquadmath -lm' )
-fi        
-declare -a COMMON_ARGS
-export COMMON_ARGS=( --no-prompt --verbosity 2 --tests main --enable-relocatable )
+    export STATIC="-static"
+    export ADD_ARGS+=( --static --with-lapack='-llapack -lblas -lgfortran -lquadmath -lm' )
+fi
+if [ x$CXX_FLAGS != x ]; then
+    export ADD_ARGS+=( CXXFLAGS=${CXXFLAGS} )
+fi
+if [ x$LD_FLAGS != x ]; then
+    export ADD_ARGS+=( LDFLAGS=${LDFLAGS} )
+fi
+export COMMON_ARGS=( --no-prompt --verbosity ${VERBOSITY:2} --tests main --enable-relocatable )
 export PLATFORM=$TRAVIS_OS_NAME${OSX:-}-x86_64-$CC$CCVERSION
 export PROJECT_URL=https://github.com/$TRAVIS_REPO_SLUG
 if [ $TRAVIS_OS_NAME = windows ]; then

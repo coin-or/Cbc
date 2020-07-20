82fc2e1acc57fbaadb2915534cbe9db05d718e4d
Stefan Vigerske <svigerske@gams.com>
2020-07-20 18:39:20 +0200

    use static_cast for static cast, fixes #319

--- src/CbcModel.cpp
+++ src/CbcModel.cpp
@@ -5576,7 +5576,7 @@ void CbcModel::branchAndBound(int doStatistics)
     OsiClpSolverInterface *clpSolver
       = dynamic_cast< OsiClpSolverInterface * >(solver_);
     if (clpSolver)
-      clpSolver->setFakeObjective(reinterpret_cast< double * >(NULL));
+      clpSolver->setFakeObjective(static_cast< double * >(NULL));
   }
 #endif
   moreSpecialOptions_ = saveMoreSpecialOptions;

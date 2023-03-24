1505e65969d57eb3951961b3963a85c3f4e44562
Jan-Willem Goossens <jhmgoossens@hotmail.com>
2023-03-24 17:45:57 +0100

    Add miplib to windows-ci testing for MSVS build

--- MSVisualStudio/v17/CbcTest.cmd
+++ MSVisualStudio/v17/CbcTest.cmd
@@ -7,8 +7,8 @@ SET "NETLIBDIR=%~3"
 SET "MIPLIBDIR=%~4"
 
 echo INFO: Running %0 %*
-echo INFO: Using bindir '%BINDIR%' and sampledir '%SAMPLEDIR%'
-echo INFO: Netlibdir '%NETLIBDIR%' and miplibdir '%MIPLIBDIR%' are ignored for these tests.
+echo INFO: Using bindir '%BINDIR%', sampledir '%SAMPLEDIR%' and miplibdir '%MIPLIBDIR%'. 
+echo INFO: Netlibdir '%NETLIBDIR%' is ignored for these tests.
 
 if "%BINDIR%"=="" echo ERROR: No bindir given. && goto :usage
 if not exist "%BINDIR%" echo ERROR: Folder bindir %BINDIR% does not exist. && goto :usage
@@ -17,14 +17,22 @@ if "%SAMPLEDIR%"=="" echo ERROR: No sampledir given. && goto :usage
 if not exist %SAMPLEDIR% echo ERROR: Folder sampledir %SAMPLEDIR% does not exist. && goto :usage
 if not %errorlevel%==0 echo ERROR: %SAMPLEDIR% cannot contain spaces. && goto :usage
 
+@REM if "%NETLIBDIR%"=="" echo ERROR: No netlibdir given. && goto :usage
+@REM if not exist %NETLIBDIR% echo ERROR: Folder netlibdir %NETLIBDIR% does not exist. && goto :usage
+@REM if not %errorlevel%==0 echo ERROR: %NETLIBDIR% cannot contain spaces. && goto :usage
+
+if "%MIPLIBDIR%"=="" echo ERROR: No miplibdir given. && goto :usage
+if not exist %MIPLIBDIR% echo ERROR: Folder miplibdir %MIPLIBDIR% does not exist. && goto :usage
+if not %errorlevel%==0 echo ERROR: %MIPLIBDIR% cannot contain spaces. && goto :usage
+
 goto :test
 
 :usage
-echo INFO: Usage %0 ^<bindir^> ^<sampledir^>
-echo INFO: where ^<bindir^> contains the executables, sampledir the sample files.
+echo INFO: Usage %0 ^<bindir^> ^<sampledir^> ^<netlibdir^> ^<miplibdir^>
+echo INFO: where ^<bindir^> contains the executables, sampledir the sample files, netlibdir the netlib files, and miplibdir the miplib files.
 echo INFO: This script runs automated test.
-echo INFO: The ^<sampledir^> must not contain spaces!
-echo INFO: For example: %0 "D:\Some Directory\" ..\..\samples\
+echo INFO: The sampledir, netlibdir and miplibdir must not contain spaces!
+echo INFO: For example: %0 "D:\Some Directory\" ..\..\samples\ . ..\..\data-miplib
 goto :error
 
 :error
@@ -42,7 +50,7 @@ echo INFO: Starting Tests
 "%BINDIR%\osiUnitTest.exe" -mpsDir=%SAMPLEDIR%
 @if not %errorlevel%==0 goto :error
 
-"%BINDIR%\cbc.exe" -dirSample %SAMPLEDIR% -unitTest
+"%BINDIR%\cbc.exe" -dirMiplib %MIPLIBDIR% -unitTest
 @if not %errorlevel%==0 goto :error
 
 @echo off

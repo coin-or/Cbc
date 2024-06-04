ca088df34881ef0d58124e53b3d70bfa73e92713
Ron @ SWGY <125934174+ron-at-swgy@users.noreply.github.com>
2024-06-04 11:10:13 -0400

    Replace reinterpret_cast of NULL with static_cast
    On FreeBSD and OpenBSD when using clang, `NULL` is a typedef to `nullptr`.
    
    This same issue is described back in July of 2020 in #319

--- src/CbcModel.cpp
+++ src/CbcModel.cpp
@@ -6255,7 +6255,7 @@ void CbcModel::branchAndBound(int doStatistics)
     OsiClpSolverInterface *clpSolver =
         dynamic_cast<OsiClpSolverInterface *>(solver_);
     if (clpSolver)
-      clpSolver->setFakeObjective(reinterpret_cast<double *>(NULL));
+      clpSolver->setFakeObjective(static_cast<double *>(NULL));
   }
 #endif
   moreSpecialOptions_ = saveMoreSpecialOptions;

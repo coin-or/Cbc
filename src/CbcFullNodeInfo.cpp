f46829db737d6f284a1b3232fdc92bb6fa42e60c
André Apitzsch <andre.apitzsch@etit.tu-chemnitz.de>
2024-07-31 11:44:00 +0200

    Remove 'using namespace std;'

--- src/CbcFullNodeInfo.cpp
+++ src/CbcFullNodeInfo.cpp
@@ -41,7 +41,6 @@
 #include "OsiClpSolverInterface.hpp"
 #include "ClpSimplexOther.hpp"
 #endif
-using namespace std;
 #include "CglCutGenerator.hpp"
 
 CbcFullNodeInfo::CbcFullNodeInfo()
--- src/CbcNodeInfo.cpp
+++ src/CbcNodeInfo.cpp
@@ -41,7 +41,6 @@
 #include "OsiClpSolverInterface.hpp"
 #include "ClpSimplexOther.hpp"
 #endif
-using namespace std;
 #include "CglCutGenerator.hpp"
 #include "CbcNodeInfo.hpp"
 
